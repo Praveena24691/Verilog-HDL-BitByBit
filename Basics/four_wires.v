b9dcd6f13d8b0ceaf59a5f7f3b4a8e0a125af2f1
Praveena Reddy <pallapraveena413@gmail.com>
2026-01-08 21:59:30 +0530

    Create four_wires.v

---
+++ Basics/four_wires.v
@@ -0,0 +1,24 @@
+/*
+PBLM:Create a module with 3 inputs and 4 outputs that behaves like wires that makes these connections:
+a -> w
+b -> x
+b -> y
+c -> z
+*/
+module wire4(
+  input a,b,c,
+  output w,x,y,z
+);
+  assign w=a;
+  assign x=b;
+  assign y=b;
+  assign z=c;
+endmodule
+
+// OR//
+module wire4(
+  input a,b,c,
+  output w,x,y,z
+);
+  assign{w,x,y,z}={a,b,b,c};
+endmodule        
